901e5ca02239d934f5cbdd4ff41c4252ef8c7edb
serena3151 <77529411+serena3151@users.noreply.github.com>
2023-01-12 13:26:52 +1100

    Add files via upload

---
+++ random.py
@@ -0,0 +1,69 @@
+import random
+
+response=input("press y to roll again and n to exit:") 
+print("\n")
+
+while response == "y": 
+ no = random.randint(1,6)
+if no == 1: 
+ print("[-----]") 
+ print("[ ]")
+ print("[ 0 ]")
+ print("[ ]")
+ print("[-----]") 
+
+response=input("press y to roll again and n to exit:") 
+print("\n")
+
+if no == 2: 
+ print("[-----]") 
+ print("[ 0 ]") 
+ print("[ ]") 
+ print("[ 0 ]") 
+ print("[-----]") 
+ 
+response=input("press y to roll again and n to exit:") 
+print("\n")
+ 
+if no == 3: 
+ print("[-----]")
+ print("[ ]")
+ print("[0  0  0]")
+ print("[ ]")
+ print("[-----]")
+ 
+response=input("press y to roll again and n to exit:") 
+print("\n")
+
+if no == 4: 
+ print("[-----]") 
+ print("[ 0  0 ]") 
+ print("[ ]") 
+ print("[ 0  0 ]") 
+ print("[-----]") 
+ 
+response=input("press y to roll again and n to exit:") 
+print("\n")
+
+if no == 5: 
+ print("[-----]") 
+ print("[ 0  0 ]") 
+ print("[   0  ]") 
+ print("[ 0  0 ]") 
+ print("[-----]") 
+ 
+response=input("press y to roll again and n to exit:") 
+print("\n")
+
+
+if no == 6:
+ print("[-----]") 
+ print("[ 0  0  0 ]") 
+ print("[ ]") 
+ print("[ 0  0  0 ]") 
+ print("[-----]") 
+
+response=input("press y to roll again and n to exit:") 
+print("\n")
+
+
